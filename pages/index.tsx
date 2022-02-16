68092c3e8c8c2e1c11c9b42144252762e0ac6c49
ThePrismDAO <greed.games@protonmail.com>
2022-02-15 16:05:11 -0800

    default tokens to mint to 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ function Home() {
   const [barWidth, setBarWidth] = useState("2%");
   const [numTokensMinted, setTotalSupply] = useState(11);
   const [numTokensAvailable, setMaxSupply] = useState(3333);
-  const [numTokensToMint, setNumTokensToMint] = useState(10);
+  const [numTokensToMint, setNumTokensToMint] = useState(3);
   const [mintPriceEth, setMintPrice] = useState(0.001);
   const [prismDAOMembershipEtherscan, setPrismDAOMembershipEtherscan] = useState("");
   const [prismDAOMembershipContractAddress, setPrismDAOMembershipContractAddress] = useState("");
